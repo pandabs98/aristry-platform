47ce42f88d4051aa66e94ab7f996082f2ee003f4
deva panda <pandabs98@gmail.com>
2025-06-09 12:01:05 +0530

    helmet setup done

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
+import helmet from "helmet"
 
 const app = express()
 app.use(cors(
@@ -9,10 +10,15 @@ app.use(cors(
         credentials:true
     }
 ))
+app.use(helmet())
 app.use(express.json({limit:"16kb"}))
 app.use(express.urlencoded({extended:true,limit:"16kb"}))
 app.use(express.static("public"))
 app.use(cookieParser())
+app.use((req, res, next) => {
+  res.status(404).json({ success: false, message: "Route not found" });
+});
+
 
 
 // Routes import
